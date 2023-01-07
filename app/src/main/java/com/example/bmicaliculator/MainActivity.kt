e159da977e88cfeee180ca562c0147adedfe8040
yuvraj <yuvrajsaini918@gmail.com>
2023-01-07 13:42:56 +0530

    finish()

--- app/src/main/java/com/example/bmicaliculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicaliculator/MainActivity.kt
@@ -11,6 +11,7 @@ import java.math.RoundingMode
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
 
